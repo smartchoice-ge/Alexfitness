d9b5a2b6efec07a875a7ed5765dd0d734fb0eefb
vj1235432 <vaso@smartchoice.ge>
2026-04-02 18:16:00 +0400

    fff

--- agreement_template.php
+++ agreement_template.php
@@ -49,36 +49,26 @@
 			<p>2.5. შპს „Synergy“ პასუხს არ აგებს წევრის მდგომარეობაზე, რომელიც გამოწვეულია სპორტული კვების ან საკვები დანამატების გამოყენებით. წევრი კისრულობს ვალდებულებას გაიაროს კონსულტაციები ექიმთან საკვები დანამატების გამოყენებამდე.</p>
 			<p>3. <b>კლუბის წევრების დაშვება</b>. კლუბის წევრებზე გაიცემა დაშვებისათვის ბარათი ან დამატებით QR კოდი, რომლებიც საშუალებას მისცემს წევრებს შევიდნენ შპს „Synergyს“ ტერიტორიაზე და ისარგებლონ აღჭურვილობით. კლუბის წევრი თანახმაა, არ მისცეს სხვა პირებს შენობებში თავისი ბარათის ან QR კოდის მეშვეობით შესვლის ნება  და არ დაუშვას, რომ მასთან ერთად სხვა პირი შევიდეს შენობაში. თუ კლუბის წევრი დაარღვევს წევრების დაშვების რაიმე პირობებს, მას შეიძლება გაუუქმდეს წევრობა, ყოველგვარი ანაზღაურების გარეშე და შეიძლება აღიძრას სისხლის სამართლის საქმე მის წინააღმდეგ. ბარათის შეცვლის ღირებულება განისაზღვრება მიმდინარე ფიქსირებული ტარიფის შესაბამისად. შპს „Synergyს“ კლუბის წევრად  გახდომისას პირს შეიძლება გადაუღონ ფოტო, რომელიც უსაფრთხოების მიზნებისთვის და მონიტორინგისთვის გამოიყენება.</p>
 			<p>4. <b>აღჭურვილობის გამოყენება</b>. ჩვენ ვთხოვთ კლუბის წევრებს ვარჯიშის დასრულების შემდეგ ადგილზე დააბრუნონ გირები და სხვა ნებისმიერი სავარჯიშო აღჭურვილობა (ფილები, ხარიხები, ჰანტელები, და ა შ.).</p>
-			<p>5. <b>აბონიმენტის ნაწილ-ნაწილ გადახდა.</b></p>
-            <p>5.1 ნაწილ-ნაწილ გადახდა შესაძლებელია გავრცელდეს მხოლოდ 12 (თორმეტი) თვიან აბონიმენტზე.</p>
-            <p>5.2 მისი ფასი განისაზღვრება შპს „Synergyს“ მიერ და შესაძლებელია შეიცვალოს ნებისმიერ დროს.</p>
-            <p>5.3 კლუბის წევრის მიერ უკვე შესყიდული აბონიმენტის ფასის ცვლილება შეუძლებელია.</p>
-            <p>5.4 მთლიანი აბონიმენტის ფასი იყოფა 5 ტოლ ნაწილად, მიღებული თანხით ხდება მისი დაფარვა ყიდვიდან შემდეგი 5 თვის განმავლობაში.</p>
-            <p>5.5 აბონიმენტის თანხა უნდა დაიფაროს აბონიმენტის ყიდვიდან შემდეგი 5 თვის განმავლობაში  არაუგვინეს  ყოველი თვის იმ დღისა, როდესაც მოხდა მომხმარებლის მიერ ხელშეკრულების დადასტურება ონლაინ საშუალებით თუ ხელშეკრულებაზე უშუალოდ ხელმოწერით.</p>
-            <p>5.6 ხუთ კალენდარულ დღეზე მეტი დაგვიანების შემთხვევაში კონტრაქტი გაუქმდება და გადახდილი თანხა უკან არ დაბრუნდება.</p>
-            <p>5.7 აღნიშნული აბონიმენტის დაპაუზება ან გაუქმება არ ხდება.</p>
-            <p>6. <b>კარადები</b>. კარადები ხელმისაწვდომია და განთავსებულია გასახდელებში. წევრი ვალდებულია ყურადღება მიაქციოს წევრის პირად ნივთებს. შპს „Synergyს“ თანამშრომლები და კონტრაქტორები პასუხს არ აგებენ პირადი ნივთების დაკარგვისთვის. კარადები არ წარმოადგენს მოწყობილობებს უსაფრთხოდ შენახვისთვის. შესაბამისად, წევრი ვალდებულია კლუბის მომსახურებით სარგებლობისას თან იქონიოს ყველა ფასეული ნივთი. წინააღმდეგ შემთხვევაში შპს
+			<p>5. <b>კარადები</b>. კარადები ხელმისაწვდომია და განთავსებულია გასახდელებში. წევრი ვალდებულია ყურადღება მიაქციოს წევრის პირად ნივთებს. შპს „Synergyს" თანამშრომლები და კონტრაქტორები პასუხს არ აგებენ პირადი ნივთების დაკარგვისთვის. კარადები არ წარმოადგენს მოწყობილობებს უსაფრთხოდ შენახვისთვის. შესაბამისად, წევრი ვალდებულია კლუბის მომსახურებით სარგებლობისას თან იქონიოს ყველა ფასეული ნივთი. წინააღმდეგ შემთხვევაში შპს
 „Synergy“ პასუხს არ აგებს ასეთი ნივთების დაკარგვაზე ან/და დაზიანებაზე.</p>
-			<p>7. <b>პირდაპირი მარკეტინგის მიზნებისთვის პერსონალურ მონაცემთა დამუშავებაზე თანხმობა</b></p>
-			<p>7.1. წინამდებარე ხელშეკრულებაზე ხელმოწერით წევრი თანხმობას აცხადებს, რომ შპს „Synergyმა“ (ს/ნ XXXXXXXXX) მისი პირადი მონაცემები, კერძოდ სახელი (სახელები), მისამართი, ტელეფონის ნომერი, ელექტრონული ფოსტის მისამართი დაამუშაოს პირდაპირი მარკეტინგის მიზნებისათვის და აღნიშნულ მონაცემებზე გაუგზავნოს და მიაწოდოს ინფორმაცია შპს „Synergy-ში“ (ს/ნ XXXXXXXXX) არსებული, დაგეგმილი ან/და	სამომავლო შეთავაზებების, რეკლამების, ფასდაკლებების, აქციებისა და ღონისძიებების შესახებ. Ასევე საინფორმაციო შინაარსის შეტყობინების გაგზავნის მიზნით.</p>
-			<p>7.2. წევრისთვის ცნობილია, რომ ნებისმიერ დროს შეუძლია უარი განაცხადოს და უკან გამოითხოვოს წინამდებარე დოკუმენტით გაცხადებული თანხმობა მისი პირადი მონაცემების პირდაპირი მარკეტინგის მიზნებისათვის დამუშავებასთან დაკავშირებით და აღნიშნული უარი შეუძლია განაცხადოს წერილობითი ფორმით, შპს
+			<p>6. <b>პირდაპირი მარკეტინგის მიზნებისთვის პერსონალურ მონაცემთა დამუშავებაზე თანხმობა</b></p>
+			<p>6.1. წინამდებარე ხელშეკრულებაზე ხელმოწერით წევრი თანხმობას აცხადებს, რომ შპს „Synergyმა" (ს/ნ XXXXXXXXX) მისი პირადი მონაცემები, კერძოდ სახელი (სახელები), მისამართი, ტელეფონის ნომერი, ელექტრონული ფოსტის მისამართი დაამუშაოს პირდაპირი მარკეტინგის მიზნებისათვის და აღნიშნულ მონაცემებზე გაუგზავნოს და მიაწოდოს ინფორმაცია შპს „Synergy-ში" (ს/ნ XXXXXXXXX) არსებული, დაგეგმილი ან/და	სამომავლო შეთავაზებების, რეკლამების, ფასდაკლებების, აქციებისა და ღონისძიებების შესახებ. Ასევე საინფორმაციო შინაარსის შეტყობინების გაგზავნის მიზნით.</p>
+			<p>6.2. წევრისთვის ცნობილია, რომ ნებისმიერ დროს შეუძლია უარი განაცხადოს და უკან გამოითხოვოს წინამდებარე დოკუმენტით გაცხადებული თანხმობა მისი პირადი მონაცემების პირდაპირი მარკეტინგის მიზნებისათვის დამუშავებასთან დაკავშირებით და აღნიშნული უარი შეუძლია განაცხადოს წერილობითი ფორმით, შპს
 „Synergyსათვის“
 (ს/ნ XXXXXXXXX) აღნიშნულის შესახებ იმავე კომუნიკაციის საშუალებით, რა საშუალებითაც მიიღებს ინფორმაციას.</p>
-			<p>8. <b>წევრთა უფლებები</b>. შპს „Synergyს“ კლუბის წევრობა წევრს უფლებას აძლევს ივარჯიშოს შეძენილი აბონიმენტით განსაზღვრული ვადით და პირობით.</p>
-			<p>9. <b>წევრის ვალდებულებები</b>. შპს „Synergyს“ კლუბის წევრს მოეთხოვება:</p>
-			<p>9.1. დაიცვას კლუბის ყველა წესი და წინამდებარე ხელშეკრულების პირობები;</p>
-			<p>9.2. გადაიხადოს ყველა შეთანხმებული საფასური;</p>
-			<p>9.3. წინასწარ აცნობოს შპს „Synergyს“ თუ არსებობს რაიმე სახის დაავადება ან რისკი, კლუბის წევრის ჯანმრთელობისთვის.</p>
-			<p>10. <b>აკრძალული მოქმედებები</b>. შენობაში აკრძალულია მოწევა, ალკოჰოლის მიღება და ნარკოტიკების (მათ შორის სტეროიდების) მოხმარება, დაუშვებელია რაიმე სახის იარაღის შეტანა. კლუბის წევრი თანახმაა არ განახორციელოს რაიმე მოქმედებები შპს „Synergyს“ შენობაში, ისეთი ნივთიერებების ზემოქმედების დროს, რომლებსაც შეუძლია დააქვეითოს მისი უნარი, მართოს აღჭურვილობა.</p>
-			<p>11. <b>შეწყვეტა</b>. კლუბის წევრობა შეიძლება შეწყდეს ვადამდე, წევრის ხანგრძლივი ავადმყოფობის ან ფიზიკური მდგომარეობის გამო. შპს
+			<p>7. <b>წევრთა უფლებები</b>. შპს „Synergyს" კლუბის წევრობა წევრს უფლებას აძლევს ივარჯიშოს შეძენილი აბონიმენტით განსაზღვრული ვადით და პირობით.</p>
+			<p>8. <b>წევრის ვალდებულებები</b>. შპს „Synergyს" კლუბის წევრს მოეთხოვება:</p>
+			<p>8.1. დაიცვას კლუბის ყველა წესი და წინამდებარე ხელშეკრულების პირობები;</p>
+			<p>8.2. გადაიხადოს ყველა შეთანხმებული საფასური;</p>
+			<p>8.3. წინასწარ აცნობოს შპს „Synergyს" თუ არსებობს რაიმე სახის დაავადება ან რისკი, კლუბის წევრის ჯანმრთელობისთვის.</p>
+			<p>9. <b>აკრძალული მოქმედებები</b>. შენობაში აკრძალულია მოწევა, ალკოჰოლის მიღება და ნარკოტიკების (მათ შორის სტეროიდების) მოხმარება, დაუშვებელია რაიმე სახის იარაღის შეტანა. კლუბის წევრი თანახმაა არ განახორციელოს რაიმე მოქმედებები შპს „Synergyს" შენობაში, ისეთი ნივთიერებების ზემოქმედების დროს, რომლებსაც შეუძლია დააქვეითოს მისი უნარი, მართოს აღჭურვილობა.</p>
+			<p>10. <b>შეწყვეტა</b>. კლუბის წევრობა შეიძლება შეწყდეს ვადამდე, წევრის ხანგრძლივი ავადმყოფობის ან ფიზიკური მდგომარეობის გამო. შპს
 „Synergy“ უფლებას იტოვებს შეწყვიტოს წევრის კონტრაქტი, შემდეგი პირობების შესაბამისად:</p>
-			<p>11.1. წევრის შეუსაბამობა წესებში ან/და ხელშეკრულებაში მითითებულ რომელიმე კრიტერიუმთან;</p>
-			<p>11.2. კლუბის წევრის ან მისი თანამშრომლების (ან კონტრაქტორების) საჩივრის საფუძველზე.</p>
-			<p>12. <b>აბონიმენტის საფასურის დაბრუნება და დაპაუზება</b>. კლუბის წევრის
-Მოთხოვნის საფუძველზე შესაძლებელია საბანკო ანგარიშზე დაბრუნდეს მხოლოდ ფასდაუკლებელი ტარიფით შეძენილი აბონიმენტის საფასური. არ რა შემთხვევაშიც გადახდილ თანხას აკლდება ყველა დაფიქსირებულის ვიზიტი, თოთოეული 25 ლარის ოდენობით (ეს საფასური შესაძლოა შეიცვალოს მიმდინარე ფიქსირებული ტარიფის შესაბამისად). დაბრუნება ხდება მოთხოვნიდან და საბანაკო ანგარიშის წარდგენიდან 7 (შვიდი) სამუშაო დღის ვადაში. აბონიმენტის ვადის დაპაუზება კი ხდება მხოლოდ კვალიფიციური დაწესებულების ჯანმრთელობის ცნობის საფუძველზე, დოკუმენტში მითითებული საავადმყოფოს ფურცელზე ყოფნის დღეების შესაბამისად, რის საფუძველზე დაუშვებელი იყო ფიზიკური დატვირთვა.</p>
-			<p>13. <b>პასუხისმგებლობა</b>. შპს „Synergy“ არ აგებს პასუხს მისგან დამოუკიდებელი მიზეზების გამო ხელშეკრულების პირობების შეუსრულებლობაზე.</p>
-			<p>14. <b>ეგზემპლარები</b>. კონტრაქტი შეიძლება გაფორმდეს ორ ან მეტ ეგზემპლარად, რომელთაგან თითოეული ორიგინალად უნდა ჩაითვალოს, ყველა ეგზემპლარი, ერთად, წარმოადგენს ერთსა და იმავე შეთანხმებას.</p>
-			<p>15.<b>მარეგულირებელი კანონმდებლობა</b>. ხელშეკრულება და ყველა მითითებული დოკუმენტები რეგულირდება საქართველოს კანონმდებლობით.
+			<p>10.1. წევრის შეუსაბამობა წესებში ან/და ხელშეკრულებაში მითითებულ რომელიმე კრიტერიუმთან;</p>
+			<p>10.2. კლუბის წევრის ან მისი თანამშრომლების (ან კონტრაქტორების) საჩივრის საფუძველზე.</p>
+			<p>11. <b>პასუხისმგებლობა</b>. შპს „Synergy" არ აგებს პასუხს მისგან დამოუკიდებელი მიზეზების გამო ხელშეკრულების პირობების შეუსრულებლობაზე.</p>
+			<p>12. <b>ეგზემპლარები</b>. კონტრაქტი შეიძლება გაფორმდეს ორ ან მეტ ეგზემპლარად, რომელთაგან თითოეული ორიგინალად უნდა ჩაითვალოს, ყველა ეგზემპლარი, ერთად, წარმოადგენს ერთსა და იმავე შეთანხმებას.</p>
+			<p>13.<b>მარეგულირებელი კანონმდებლობა</b>. ხელშეკრულება და ყველა მითითებული დოკუმენტები რეგულირდება საქართველოს კანონმდებლობით.
 <b>ვადასტურებ, რომ წავიკითხე კონტრაქტი, ჩემთვის გასაგებია ყველა პირობა და ვეთანხმები მათ. ასევე, ვადასტურებ, რომ გადმომეცა წევრის კონტრაქტის სრულად გაფორმებული ეგზემპლარი.</b></p>
 			<br/>
 			<p><b>კლუბის წევრის ხელმოწერა</b> </p>
@@ -121,32 +111,23 @@
 			<p>2.5. 'Synergy' LLC is not responsible for the condition of the member, which is caused from sport  feeding  or  using  the  dietary  supplements.  The  member  undertakes  the  obligation  to consult with doctors before using the dietary supplements.</p>
 			<p>3. <p>Admission of club members.</p> The special access cards or additional QR code will be issued for the club members in order to admit them at the territory of “Synergy” LLC and use by equipment. The member of the club is agreed, not to give the permit to other persons though his own access card or QR code to access the building and not to allow to enter the other persons  with  him  the  building.     If  the  member  of  the  club  breaches  any  condition  of members access, his membership may be annulled, without any compensation  and criminal case may be initiated against him.  The cost of changing the card is determined according to the current fixed tariff.   After being the member of the club of “Synergy” LLC the person may be taken a photo, which will be used for the purposed of safety and monitoring.</p>
 			<p>4. <p>Usage of equipment.</p> We ask the member of the club after ending of the exercise to return the kettlebells and any other exercise equipment (plates, bars, dumbbells, etc.).</p>
-			<p>5. <b>Payment of the subscription in installments.</b></p>
-            <p>5.1 Payment in installments can only be applied to 12 (twelve) month subscriptions.</p>
-            <p>5.2 Its price is determined by “Synergy” LLC and may change at any time.</p>
-            <p>5.3 It is not possible to change the price of a subscription already purchased by a club member.</p>
-            <p>5.4 The total subscription price is divided into 5 equal parts, and the amount received is used to pay for it within 5 months after purchase.</p>
-            <p>5.5 The subscription price must be paid within 5 months of the subscription purchase, no later than the day of  each month when the user  confirms  the contract online or  by directly signing the contract.</p>
-            <p>5.6 In case of delay of more than five calendar days, the contract will be annulled   and the amount paid will not be refunded.</p>
-            <p>5.7 This subscription cannot be paused or annulled.</p>
-            <p>6. <p>Lockers.</p> Lockers are available and located in the changing rooms. The member is obliged to pay attention to the member's personal belongings. Employees and contractors of “Synergy” LLC are not responsible for the loss of personal belongings. Lockers are not devices for safe storage. Accordingly, the member is obliged to bring all valuables with him when using the club's services. Otherwise, “Synergy” LLC is not responsible for the loss and/or damage of such items.</p>
-			<p>7. <b>Consent to the processing of personal data for direct marketing purposes</b></p>
-			<p>7.1. By  signing  this  Contract,  the  Member  agrees  that  “Synergy”  LLC  (Identification  No. XXXXXXXXX) may process his personal data, namely name(s), address, telephone number, e-mail address, for direct marketing purposes and to send and provide information on the above data about existing, planned and/or future offers, advertisements, discounts, promotions and events of “Synergy” LLC (Identification No. XXXXXXXXX). Also for the purpose of sending informational messages.</p>
-			<p>7.2. The Member is aware that at any time he may refuse and withdraw the consent expressed in this document regarding the processing of his personal data for direct marketing purposes and may express this refusal in writing to Synergy LLC (Identification No. XXXXXXXXX) via the same communication channel through which he receives the information.</p>
-			<p>8. <p>Member Rights.</p> Membership in the “Synergy”  LLC club entitles the member to exercise for the period and under the conditions specified in the purchased subscription.</p>
-			<p>9. <p>Member Obligations.</p> A member of the “Synergy” LLC club is required to:</p>
-			<p>9.1. to comply with all club rules and the terms of this Contract;</p>
-			<p>9.2. to pay all agreed fees;</p>
-			<p>9.3. to inform “Synergy” LLC in advance if there is any type of illness or risk to the health of the club member.</p>
-			<p>10. <b>Prohibited Activities</b>. Smoking, alcohol consumption, and drug use (including steroids) are prohibited in the building, and it is not permitted to bring any type of weapon into the building. The member of the club agrees not to perform any activities in the premises of 'Synergy' LLC while under the influence of substances that may impair his ability to operate equipment.</p>
-			<p>11. <b>Termination</b>. Club  membership  may  be  terminated  early  due  to  a  member's  prolonged
+			<p>5. <p>Lockers.</p> Lockers are available and located in the changing rooms. The member is obliged to pay attention to the member's personal belongings. Employees and contractors of "Synergy" LLC are not responsible for the loss of personal belongings. Lockers are not devices for safe storage. Accordingly, the member is obliged to bring all valuables with him when using the club's services. Otherwise, "Synergy" LLC is not responsible for the loss and/or damage of such items.</p>
+			<p>6. <b>Consent to the processing of personal data for direct marketing purposes</b></p>
+			<p>6.1. By  signing  this  Contract,  the  Member  agrees  that  "Synergy"  LLC  (Identification  No. XXXXXXXXX) may process his personal data, namely name(s), address, telephone number, e-mail address, for direct marketing purposes and to send and provide information on the above data about existing, planned and/or future offers, advertisements, discounts, promotions and events of "Synergy" LLC (Identification No. XXXXXXXXX). Also for the purpose of sending informational messages.</p>
+			<p>6.2. The Member is aware that at any time he may refuse and withdraw the consent expressed in this document regarding the processing of his personal data for direct marketing purposes and may express this refusal in writing to Synergy LLC (Identification No. XXXXXXXXX) via the same communication channel through which he receives the information.</p>
+			<p>7. <p>Member Rights.</p> Membership in the "Synergy"  LLC club entitles the member to exercise for the period and under the conditions specified in the purchased subscription.</p>
+			<p>8. <p>Member Obligations.</p> A member of the "Synergy" LLC club is required to:</p>
+			<p>8.1. to comply with all club rules and the terms of this Contract;</p>
+			<p>8.2. to pay all agreed fees;</p>
+			<p>8.3. to inform "Synergy" LLC in advance if there is any type of illness or risk to the health of the club member.</p>
+			<p>9. <b>Prohibited Activities</b>. Smoking, alcohol consumption, and drug use (including steroids) are prohibited in the building, and it is not permitted to bring any type of weapon into the building. The member of the club agrees not to perform any activities in the premises of 'Synergy' LLC while under the influence of substances that may impair his ability to operate equipment.</p>
+			<p>10. <b>Termination</b>. Club  membership  may  be  terminated  early  due  to  a  member's  prolonged
 				illness  or  physical  condition.  “Synergy”  LLC  reserves  the  right  to  terminate  a  member's contract in accordance with the following conditions:</p>
-			<p>11.1. Non-compliance of the member with any of the criteria specified in the Rules and/or contract;</p>
-			<p>11.2. Based on a complaint from a club member or its employees (or contractors).</p>
-			<p>12. <b>Refund and pausing of subscription fees</b>. Upon request of a club member, only the fee for a subscription purchased at a non-discounted rate may be refunded to the bank account.    In no case will the amount paid be deducted from all registered visits, in the amount of GEL 25 each (this fee may change in accordance with the current fixed tariff). Refunds are made within 7 (seven) working days of the request and submission of a bank statement.  The subscription period can only be paused based on a health certificate from a qualified institution, in accordance with the days of stay on the hospital sheet indicated in the document, based on which physical activity was not allowed.</p>
-			<p>13. <b>Responsibility</b>. 'Synergy' LLC is not responsible for failure to fulfill the terms of the contract due to reasons beyond its control</p>
-			<p>14. <b>Copies</b>. A contract may be executed in two or more counterparts, each of which shall be deemed as an original, all counterparts, together constituting one and the same agreement.</p>
-			<p>15.<b>Governing Law</b>. The Contract and all specified documents are governed by the legislation of Georgia.
+			<p>10.1. Non-compliance of the member with any of the criteria specified in the Rules and/or contract;</p>
+			<p>10.2. Based on a complaint from a club member or its employees (or contractors).</p>
+			<p>11. <b>Responsibility</b>. 'Synergy' LLC is not responsible for failure to fulfill the terms of the contract due to reasons beyond its control</p>
+			<p>12. <b>Copies</b>. A contract may be executed in two or more counterparts, each of which shall be deemed as an original, all counterparts, together constituting one and the same agreement.</p>
+			<p>13.<b>Governing Law</b>. The Contract and all specified documents are governed by the legislation of Georgia.
 				I confirm that I have read the contract, understand all the terms and conditions, and agree to them. I also confirm that I have been given a fully executed copy of the member contract.
 				</b></p>
 			<br/>
